7e9e8e66eeec3457c0a5d3b2bf1a12040e401ea0
Jason Swails <jason.swails@gmail.com>
2020-05-09 23:09:07 -0400

    Add some git common functions

---
+++ vars/git.groovy
@@ -0,0 +1,124 @@
+// Common git functionality
+
+/**
+ * Returns the set of files that have changed between the current commit and
+ * a target ref (could be a branch, commit hash, tag, or anything else that
+ * git recognizes as a ref)
+ *
+ * Note, this can only be executed from a Unix agent with `git` available
+ *
+ * :param targetRef: name of the target ref to use. This is required.
+ * :param sourceRef: name of the source ref to use. Default is HEAD.
+ * :param fetchRemote: name of the remote to fetch. If not set, nothing is fetched.
+ * :param gitCredentialsId: the credentials ID to use to fetch from the remote.
+ *                          if not set, anonymous fetching will be attempted
+ * :param allChanges: if true, fetch all changes between the refs. if false, only
+ *                    the changes from the source will be listed. More explicitly,
+ *                    allChanges: true corresponds to "git diff targetRef..sourceRef"
+ *                    while allChanges: false corresponds to "git diff targetRef...sourceRef".
+ *                    Default is false
+ * :returns: set of file names that have changed between the two refs
+ */
+Set<String> diffFiles(Map params = [:]) {
+    assert params.targetRef : 'targetRef parameter is required'
+    String sourceRef = params.sourceRef ?: 'HEAD'
+    boolean allChanges = params.getOrDefault('allChanges', false)
+
+    if (params.fetchRemote) {
+        // Prune in case we re-use git repositories
+        Closure fetchCommand = { ->
+            sh(label: 'Fetch Remote', script: "git fetch --prune ${params.fetchRemote}")
+        }
+        if (params.gitCredentialsId) {
+            sshagent([params.gitCredentialsId]) {
+                fetchCommand()
+            }
+        } else {
+            fetchCommand()
+        }
+    }
+
+    String diffCommand = "${targetRef}...${sourceRef}"
+    if (allChanges) {
+        diffCommand = "${targetRef}..${sourceRef}"
+    }
+
+    return sh(
+        label: 'git diff',
+        script: """#!/bin/bash -ex
+            git diff --name-only ${diffCommand}
+        """,
+        returnStdout: true
+    ).split('\n').toSet()
+}
+
+/**
+ * Merges the target branch into the current branch. Note, this must be run on a Unix agent
+ *
+ * :param currentBranch: name of the branch to be currently checked out. Since Jenkins checks out
+ *                       in detached head state, this parameter is required
+ * :param targetBranch: name of the branch to merge into currentBranch *from the remote*. Required.
+ * :param gitCredentialsId: name of the credentials ID to use to push changes back to git
+ * :param push: true if the merged changes should be pushed (default), false otherwise
+ * :param remote: name of the remote to push to. Default is origin
+ * :returns: true if the merge made changes and false otherwise
+ */
+boolean merge(Map params = [:]) {
+    assert params.currentBranch : 'currentBranch is required'
+    assert params.targetBranch : 'targetBranch is required'
+    assert params.gitCredentialsId : 'gitCredentialsId is required'
+
+    String remote = params.remote ?: 'origin'
+    boolean doPush = params.getOrDefault('push', true)
+
+    boolean hasFailed = false
+    String errorMessage
+    String mergeContents
+
+    sshagent([params.gitCredentialsId]) {
+        hasFailed = sh(label: 'git fetch merge and push', script: """#!/bin/bash
+            set -exo pipefail
+            echo "NO MERGE" > merge_output.txt
+
+            echo "INFO: fetching from ${remote}"
+            git fetch ${remote}
+
+            echo "INFO: checking out ${params.currentBranch}"
+            git checkout -b ${params.currentBranch} --track ${remote}/${params.currentBranch}
+            git pull
+
+            echo "INFO: merging target branch"
+            git merge --no-edit ${remote}/${params.targetBranch} > merge_output.txt
+        """, returnStatus: true) != 0
+        String mergeText = readFile('merge_output.txt').trim()
+
+        if (mergeText == 'NO MERGE') {
+            assert hasFailed : 'NO MERGE could not have happened if the script passed'
+            error("Failed before merge was attempted. See job output for details")
+        } else if (hasFailed) {
+            errorMessage = "Merge failed: ${mergeText}"
+            int status = sh(label: 'abort merge', script: 'git merge --abort', returnStatus: true)
+            if (status != 0) {
+                echo "ERROR: git merge --abort failed. Ignoring..."
+            }
+            error(errorMessage)
+        } else if (mergeText == "Already up to date.") {
+            echo "INFO: ${params.currentBranch} was already up-to-date with ${params.targetBranch}"
+            return false
+        } else {
+            echo "INFO: merge made changes"
+            if (doPush) {
+                hasFailed = sh(label: 'push changes', returnStatus: true,
+                               script: "git push ${remote} ${params.currentBranch}") == 0
+                if (hasFailed) {
+                    error("Failed to push changes to ${remote}")
+                }
+            } else {
+                echo "INFO: push set to false; not pushing"
+            }
+            return true
+        }
+    }
+
+    return false
+}
