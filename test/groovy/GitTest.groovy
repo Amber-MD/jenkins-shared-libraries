83da5c1613910752a833d945e3a6f37c49e6a2df
Jason Swails <jason.swails@gmail.com>
2020-05-12 14:57:32 -0400

    Checkpointing gittest.groovy

--- test/groovy/GitTest.groovy
+++ test/groovy/GitTest.groovy
@@ -9,7 +9,7 @@ class GitTest extends BasePipelineTest {
     void setUp() {
         super.setUp()
         helper.registerAllowedMethod('echo', [String.class], {label -> println(label)})
-        helper.registerAllowedMethod('sshagent', [String.class, Closure.class], {creds, closure ->
+        helper.registerAllowedMethod('sshagent', [List.class, Closure.class], {creds, closure ->
             closure.call()
         })
     }
@@ -46,5 +46,110 @@ class GitTest extends BasePipelineTest {
         def sshAgentCalls = helper.callStack.findAll { call -> call.methodName == 'sshagent' }
         assert shCalls.size() == 2 : 'Should have made 2 sh calls'
         assert sshAgentCalls.size() == 0 : 'Should not have called sshagent'
+        assert fileList == ['file1', 'file2', 'file3'] : 'Unexpected return value for diffFiles'
+        String diffCommand = shCalls[1].args[0].script.toString()
+        assert diffCommand.contains('git diff --name-only some-ref...HEAD') : 'Bad diff command'
+    }
+
+    @Test
+    void 'diffFiles fetches the remote when requested with the given gitCredentials'() {
+        def script = loadScript('vars/git.groovy')
+        helper.registerAllowedMethod('sh', [Map.class], {params ->
+            if (params.returnStdout) {
+                if (params.label == 'git diff') {
+                    return 'file1\nfile2\nfile3'
+                }
+                return ''
+            }
+            return null
+        })
+        String[] fileList = script.diffFiles(
+            targetRef: 'some-ref',
+            fetchRemote: 'git@github.com:/Amber-MD/jenkins-shared-libraries',
+            gitCredentialsId: 'some-creds',
+            sourceRef: 'other-ref',
+            allChanges: true,
+        )
+        // Do our assertions
+        def shCalls = helper.callStack.findAll { call -> call.methodName == 'sh' }
+        def sshAgentCalls = helper.callStack.findAll { call -> call.methodName == 'sshagent' }
+        assert shCalls.size() == 2 : 'Should have made 2 sh calls'
+        assert sshAgentCalls.size() == 1 : 'Should not have called sshagent'
+        assert fileList == ['file1', 'file2', 'file3'] : 'Unexpected return value for diffFiles'
+        assert sshAgentCalls[0].args[0] == ['some-creds'] : 'Did not use the right credentials'
+        String diffCommand = shCalls[1].args[0].script.toString()
+        assert diffCommand.contains('git diff --name-only some-ref..other-ref') : 'Bad diff command'
+    }
+
+    @Test
+    void 'merge requires currentBranch'() {
+        def script = loadScript('vars/git.groovy')
+        try {
+            script.merge(targetBranch: 'target', gitCredentialsId: 'some-creds')
+            assert false : 'Should have failed'
+        } catch(AssertionError err) {
+            println('[INFO] Caught expected AssertionError')
+        }
+    }
+
+    @Test
+    void 'merge requires targetBranch'() {
+        def script = loadScript('vars/git.groovy')
+        try {
+            script.merge(sourceBranch: 'source', gitCredentialsId: 'some-creds')
+            assert false : 'Should have failed'
+        } catch(AssertionError err) {
+            println('[INFO] Caught expected AssertionError')
+        }
+    }
+
+    @Test
+    void 'merge requires gitCredentialsId'() {
+        def script = loadScript('vars/git.groovy')
+        try {
+            script.merge(currentBranch: 'source', targetBranch: 'target')
+            assert false : 'Should have failed'
+        } catch(AssertionError err) {
+            println('[INFO] Caught expected AssertionError')
+        }
+    }
+
+    @Test
+    void 'merge recognizes if no changes were made'() {
+        def script = loadScript('vars/git.groovy')
+        helper.registerAllowedMethod('readFile', [String.class], {fname ->
+            assert fname == 'merge_output.txt' : 'merge_output.txt should be read'
+            return 'Already up to date.'
+        })
+        helper.registerAllowedMethod('sh', [Map.class], {params ->
+            return 0
+        })
+        Map results = script.merge(currentBranch: 'source', targetBranch: 'target',
+                                   gitCredentialsId: 'some-creds')
+
+        def shCalls = helper.callStack.findAll {call -> call.methodName == 'sh' && call.args[0].label == 'push changes'}
+        assert shCalls.size() == 0 : 'Should not have pushed'
+        assert !results.changesMade : 'Should not have made any changes'
+        assert results.succeeded : 'Should have recorded a success'
+    }
+
+    @Test
+    void 'merge will push made changes if requested'() {
+        def script = loadScript('vars/git.groovy')
+        helper.registerAllowedMethod('readFile', [String.class], {fname ->
+            assert fname == 'merge_output.txt' : 'merge_output.txt should be read'
+            return 'Updating 9234235..123434\nFast-forward\n .gitignore | 3+--\n'
+        })
+        helper.registerAllowedMethod('sh', [Map.class], {params ->
+            return 0
+        })
+        Map results = script.merge(currentBranch: 'source', targetBranch: 'target',
+                                   gitCredentialsId: 'some-creds')
+
+        def shCalls = helper.callStack.findAll {call -> call.methodName == 'sh' && call.args[0].label == 'push changes'}
+        assert shCalls.size() == 1 : 'Should not have pushed'
+        assert results.changesMade : 'Should have made any changes'
+        assert results.succeeded : 'Should have recorded a success'
+
     }
 }
