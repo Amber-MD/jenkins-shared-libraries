81ef26d751568e9f43be1e3a6b38bd0dce4d4963
Jason Swails <jason.swails@gmail.com>
2021-01-27 14:22:52 -0500

    more debugging

--- vars/github.groovy
+++ vars/github.groovy
@@ -82,7 +82,7 @@ void interrogateBuild() {
             echo "BuildAction class name is ${it.class.name}"
             if (it.hasProperty("lastBuiltRevision")) {
                 echo "Last built revision = ${it.lastBuiltRevision} [${it.lastBuiltRevision.class.name}]"
-                echo "Last built revision sha1 = ${it.lastBuiltRevision.getSha1()}"
+                echo "Last built revision sha1 = ${it.lastBuiltRevision.getSha1()} [${it.lastBuiltRevision.getSha1().class.name}]"
             }
         }
         build = build.previousBuild
