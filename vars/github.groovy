2301b34c1566cfc870721ce07fe59ae42933bba8
Jason Swails <jason.swails@gmail.com>
2021-01-27 22:03:30 -0500

    Simplify significantly, but also means we need to allocate an executor for this check

--- vars/github.groovy
+++ vars/github.groovy
@@ -3,17 +3,13 @@
  * the files changed are available in the build information. The credentials and GitHub API will
  * only be used for branch builds where CHANGE_ID is not set.
  *
- * This check does *not* require an allocated executor.
+ * This check falls back on the git command-line for branch builds, so it requires an allocated
+ * executor and must be executed from inside a git repository that has its full history checked
+ * out.
  *
  * :param path: Name of the file or path prefix where changes should be looked for
  * :param paths: A list of file or path prefix names where changes should be looked for
- * :param credentialsId: A set of username/password credentials to access the GitHub API (needed only for private repos)
- * :param githubApiUrl: The API URL for the GitHub instance. Default is https://api.github.com
  */
-import org.eclipse.jgit.transport.RemoteConfig
-import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
-import hudson.plugins.git.util.BuildData
-
 boolean fileChangedIn(Map params = [:]) {
     String path = params.path ?: ''
     def paths = params.paths ?: []
@@ -30,18 +26,9 @@ boolean fileChangedIn(Map params = [:]) {
         pullRequest.files.each { changedFiles << it.filename }
     } else {
         echo "INFO: Not a pull request; looking for changes since the last successful build"
-        try {
-            result = filesChangedSinceLastSuccessfulBuild(params.githubApiUrl ?: "https://api.github.com", params.credentialsId ?: "")
-        } catch(Exception ex) {
-            echo "ERROR: Failed fetching change list [${ex}]. Assume something changed"
-            return true
-        }
+        result = filesChangedSinceLastSuccessfulBuild()
         if (!result.foundSuccessfulBuild) {
             echo "WARNING: Did not find a prior successful build. Forcibly saying yes"
-            return true
-        }
-        if (result.changedFiles.size() == 0) {
-            echo "WARNING: No changed files found. Forcibly saying yes"
             return true
         }
         changedFiles = result.changedFiles
@@ -59,8 +46,7 @@ boolean fileChangedIn(Map params = [:]) {
 }
 
 // Internal method for finding list of files that have changed since the last build
-Map filesChangedSinceLastSuccessfulBuild(String githubApiUrl, String credentialsId) {
-    def changedFiles = []
+Map filesChangedSinceLastSuccessfulBuild() {
     String remoteUrl = getRemoteUrl()
     String currentCommit = getRevisionFromBuild(currentBuild, remoteUrl)
     String lastCommit = getRevisionFromBuild(currentBuild.previousSuccessfulBuild, remoteUrl)
@@ -69,54 +55,12 @@ Map filesChangedSinceLastSuccessfulBuild(String githubApiUrl, String credentials
         echo "INFO: Not enough information to determine any file changes."
         return [changedFiles: changedFiles, foundSuccessfulBuild: false]
     }
-
-    Map repoDetails = getOrganizationRepoFromRemote(remoteUrl)
-
-    if (!repoDetails.organization || !repoDetails.repository) {
-        echo "ERROR: Could not find organization or repository from ${remoteUrl}"
-        return [changedFiles: changedFiles, foundSuccessfulBuild: false]
-    }
-
-    String org = repoDetails.organization
-
-    String apiUrl = "${githubApiUrl}/repos/${repoDetails.organization}/${repoDetails.repository}/compare/${lastCommit}...${currentCommit}"
-
-    Map response
-    if (credentialsId == "" || credentialsId == null) {
-        echo "INFO: Getting file changes via anonymous access"
-        def resp = httpRequest(
-            url: apiUrl.toString(),
-            acceptType: 'APPLICATION_JSON',
-            contentType: 'APPLICATION_JSON',
-            httpMode: 'GET',
-            validResponseCodes: '100:399',
-        )
-        response = readJSON(text: resp.getContent())
-    } else {
-        withCredentials([string(credentialsId: credentialsId, variable: 'GITHUB_TOKEN')]) {
-            def resp = httpRequest(
-                url: apiUrl.toString(),
-                acceptType: 'APPLICATION_JSON',
-                contentType: 'APPLICATION_JSON',
-                httpMode: 'GET',
-                validResponseCodes: '100:399',
-                customHeaders = [
-                    [name: 'Authorization', value: "token ${GITHUB_TOKEN}", maskValue: true]
-                ]
-            )
-        }
-        response = readJSON(text: resp.getContent())
-    }
-
-    (response.files ?: []).each { fileInfo ->
-        changedFiles.add(fileInfo.filename)
-    }
-
+    List<String> changedFiles = git.diffFiles(targetRef: lastCommit, sourceRef: currentCommit)
     return [changedFiles: changedFiles, foundSuccessfulBuild: true]
 }
 
 String getRemoteUrl() {
-    List<RemoteConfig> repositories = scm.getRepositories()
+    List repositories = scm.getRepositories()
     if (repositories.size() < 1) {
         echo "ERROR: No remote repository detected. Is the Jenkinsfile fetched from source?"
         return null
@@ -140,12 +84,7 @@ String getRemoteUrl() {
     return urls[0];
 }
 
-Map getOrganizationRepoFromRemote(String url) {
-    List<String> urlParts = url.replaceAll("\\.git\$", "").split("/")
-    return [organization: urlParts[urlParts.size() - 2], repository: urlParts[urlParts.size() - 1]]
-}
-
-String getRevisionFromBuild(RunWrapper build, String remoteUrl) {
+String getRevisionFromBuild(def build, String remoteUrl) {
     if (build == null) {
         echo "INFO: No build given, so unable to find revision id"
         return null
@@ -154,7 +93,7 @@ String getRevisionFromBuild(RunWrapper build, String remoteUrl) {
     String gitHash = null
 
     build.rawBuild.getActions().each { action ->
-        if (action instanceof BuildData) {
+        if (action.hasProperty("getRemoteUrls")) {
             if (action.getRemoteUrls().contains(remoteUrl)) {
                 echo "INFO: Matched SCM URL for build action"
                 gitHash = action.lastBuiltRevision.getSha1String()
@@ -168,49 +107,4 @@ String getRevisionFromBuild(RunWrapper build, String remoteUrl) {
     }
 
     return gitHash
-}
-
-Map interrogateBuild(Map params = [:]) {
-    Map results = filesChangedSinceLastSuccessfulBuild("https://api.github.com", params.credentialsId)
-    echo "filesChangedSinceLastSuccessfulBuild: ${results}"
-    return results
-
-
-    String url = getRemoteUrl()
-    String currentCommit = getRevisionFromBuild(currentBuild, url)
-    String lastCommit = getRevisionFromBuild(currentBuild.previousSuccessfulBuild, url)
-    if (url == null || currentCommit == null || lastCommit == null) {
-        echo "WARNING: Could not find all the information needed to identify changes."
-    } else {
-        echo "INFO: Should look for ${url}/${lastCommit}...${currentCommit}"
-        String noGit = url.replaceAll("\\.git\$", "")
-        echo "Without .git: ${noGit}"
-        List<String> urlParts = noGit.split("/")
-        echo "Organization = ${urlParts[urlParts.size()-2]} repo = ${urlParts[urlParts.size()-1]}"
-    }
-    /*
-    int i = 0
-    echo "scm = ${scm}"
-    echo "repositories = ${scm.getRepositories()}"
-    echo "URIs = ${scm.getRepositories()[0].getURIs()}"
-    String url = ${scm.getRepositories()[0].getURIs()[0].toString()}
-    echo "First URL = ${url}"
-    while (i < 2) {
-        echo "Build ${build.id} had result ${build.result}"
-        echo "Build variables = ${build.absoluteUrl}"
-        echo "Build actions = ${build.rawBuild.getActions()}"
-        echo "Interrogating build actions:"
-        build.rawBuild.getActions().each {
-            echo "BuildAction class name is ${it.class.name}"
-            if (it instanceof hudson.plugins.git.util.BuildData) {
-                echo "Last built revision = ${it.lastBuiltRevision} [${it.lastBuiltRevision.class.name}]"
-                echo "Last built revision sha1 = ${it.lastBuiltRevision.getSha1String()}"
-                echo "Remote URLs: ${it.getRemoteUrls()}"
-                echo "SCM Name: ${it.getScmName()}"
-            }
-        }
-        build = build.previousSuccessfulBuild
-        i++
-    }
-    */
 }
